6c3001877b09e4245465ceb9c11b88e6234310ff
SrPirson <fcp0009@alu.medac.com>
2025-10-08 18:12:11 +0200

    style: create style srollbar

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = () => {
   return (
-    <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+    <div className="absolute bottom-21 left-1/2">
       <img
         src="assets/img/arrow.gif"
         alt="Scroll arrow"
