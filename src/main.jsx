666c681be8d35efffc7af93a28bc51cc1a55fdb4
SrPirson <169342716+SrPirson@users.noreply.github.com>
2026-04-22 10:16:47 +0200

    feat: add Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import { I18nextProvider } from "react-i18next";
 import i18next from 'i18next';
-import { Analytics } from "@vercel/analytics/react";
-
 import global_es from "./locales/es/global.json";
 import global_en from "./locales/en/global.json";
 import global_fr from "./locales/fr/global.json";
@@ -48,6 +50,7 @@ createRoot(document.getElementById('root')).render(
     <I18nextProvider i18n={i18next}>
       <App />
       <Analytics />
+      <SpeedInsights/>
     </I18nextProvider>
   </StrictMode>,
 )
